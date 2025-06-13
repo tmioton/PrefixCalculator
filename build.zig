12af8b4e0bc3ea81016078e45d803f88e868d76a
Tate Mioton <tatemioton@outlook.com>
2025-06-12 20:47:29 -0500

    Simplify build.zig

--- build.zig
+++ build.zig
@@ -3,15 +3,13 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     const exe = b.addExecutable(.{
-        .name = "Calculator",
-        .root_module = exe_mod,
+        .name = "prefix",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     b.installArtifact(exe);
