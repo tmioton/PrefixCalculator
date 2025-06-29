0ece5d63b038534bd8935a5d8afc1c1602548148
Tate Mioton <tatemioton@outlook.com>
2025-06-28 19:44:09 -0500

    Export Module

--- build.zig
+++ build.zig
@@ -3,13 +3,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const prefix = b.addModule("prefix", .{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const exe = b.addExecutable(.{
         .name = "prefix",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = prefix,
     });
 
     b.installArtifact(exe);
